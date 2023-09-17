5a58adf1553366d98d97bdbfdc03319b52da6958
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2023-09-17 19:15:04 +0530

    fix: remove navbar in small screen

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,10 +9,10 @@ const Banner = () => {
   return (
     <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
       <div className="container mx-auto">
-        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12 '>
+        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-14 '>
           <div className='flex-1 text-center font-secondary lg:text-left'>
-            <motion.h1 variants={fadeIn('up', 0.3)} initial="hidden" whileInView={'show'} viewport={{once: false, amount: 0.7}}  className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>VISAKH <span>VIJAY</span></motion.h1>
-            <motion.div variants={fadeIn('up', 0.4)} initial="hidden" whileInView={'show'} viewport={{once: false, amount: 0.7}} className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
+            <motion.h1 variants={fadeIn('up', 0.3)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>VISAKH <span>VIJAY</span></motion.h1>
+            <motion.div variants={fadeIn('up', 0.4)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
               <span className=' text-white mr-4'>I am a</span>
               <TypeAnimation sequence={[
                 'Developer',
@@ -29,29 +29,30 @@ const Banner = () => {
               />
 
             </motion.div>
-            <motion.p variants={fadeIn('up', 0.5)} initial="hidden" whileInView={'show'} viewport={{once: false, amount: 0.7}} className='mb-8 max-w-lg mx-auto lg:mx-0'>
+            {/* <motion.p variants={fadeIn('up', 0.5)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='mb-8 max-w-lg mx-auto lg:mx-0'>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam exercitationem blanditiis distinctio odio, impedit assumenda quam quas doloremque nulla quibusdam.
-            </motion.p>
-            <motion.div variants={fadeIn('up', 0.6)} initial="hidden" whileInView={'show'} viewport={{once: false, amount: 0.7}} className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+            </motion.p> */}
+            <motion.div variants={fadeIn('up', 0.6)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
               <button className='btn btn-lg'> Contact Me</button>
               <a href="#" className='text-gradient btn-link'>My Portfolio</a>
             </motion.div>
             {/* socials */}
-            <motion.div variants={fadeIn('up', 0.7)} initial="hidden" whileInView={'show'} viewport={{once: false, amount: 0.7}} className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+            <motion.div variants={fadeIn('up', 0.7)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
               <a href="#">
-                <FaGithub/>
+                <FaGithub />
               </a>
               <a href="#">
-                <FaLinkedin/>
+                <FaLinkedin />
               </a>
               <a href="#">
-                <FaTree/>
+                <FaTree />
               </a>
             </motion.div>
           </div >
           {/* image */}
-          <motion.div variants={fadeIn('down', 0.5)} initial="hidden" whileInView={'show'} className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'>
-            <img src={Image} alt="" />
+          <motion.div variants={fadeIn('down', 0.5)} initial="hidden" whileInView={'show'} className='lg:flex flex-1 max-w-[320px] lg:max-w-[482px] items-center justify-center'>
+            {/* <img src={Image} alt="" /> */}
+
           </motion.div>
         </div>
       </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,19 +4,13 @@ import { BsClipboard2Data, BsBriefcase, BsChatSquare, BsSquare, BsChatSquareText
 import { Link } from 'react-scroll';
 
 const Nav = () => {
-  return <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
+  return <nav className='fixed sm:hidden lg:flex bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
     <div className="container mx-auto">
       {/* {Nav inner} */}
       <div className='wf-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between text-2xl text-white/50 items-center'>
         <Link to='home' activeClass='active' smooth={true} spy={true} offset={-200} className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
           <BiHomeAlt />
         </Link>
-        {/* <Link to='about' activeClass='active' smooth={true} spy={true} className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
-          <BiUser />
-        </Link>
-        <Link to='services' activeClass='active' smooth={true} spy={true} className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
-          <BsClipboard2Data />
-        </Link> */}
         <Link to='work' activeClass='active' smooth={true} spy={true} className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
           <BsBriefcase />
         </Link>
