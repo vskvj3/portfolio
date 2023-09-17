c451c9d830d6e4fa3c6b10d90e306b87be16c1c8
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2023-09-17 18:45:18 +0530

    fix: changed favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Logo from '../assets/logo.svg';
+import Logo from '../assets/logo.png';
 
 const Header = () => {
   return <header className=' py-8 '>
     <div className="container mx-auto">
       <div className='flex justify-between items-center'>
         <a href="">
-          <img src={Logo} alt="" />
+          <img src={Logo} alt="" width={100} height={100}/>
         </a>
 
         <button className='btn btn-sm'>
