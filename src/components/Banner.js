5c98ceeb21f2af2facd7d8dabad57e4350339000
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2023-09-17 21:37:33 +0530

    feat: add terminal

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,12 +4,27 @@ import { FaGithub, FaLinkedin, FaTree } from 'react-icons/fa';
 import { TypeAnimation } from 'react-type-animation';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../variants';
+import { useState, useEffect } from 'react';
+
+
 
 const Banner = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+useEffect(() => {
+  const timer = setTimeout(() => {
+    setIsVisible(true);
+  }, 6500); 
+
+  return () => {
+    clearTimeout(timer); 
+  };
+}, []);
+  
   return (
     <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
       <div className="container mx-auto">
-        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-14 '>
+        <div className='lg:flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-14'>
           <div className='flex-1 text-center font-secondary lg:text-left'>
             <motion.h1 variants={fadeIn('up', 0.3)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>VISAKH <span>VIJAY</span></motion.h1>
             <motion.div variants={fadeIn('up', 0.4)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
@@ -50,9 +65,26 @@ const Banner = () => {
             </motion.div>
           </div >
           {/* image */}
-          <motion.div variants={fadeIn('down', 0.5)} initial="hidden" whileInView={'show'} className='lg:flex flex-1 max-w-[320px] lg:max-w-[482px] items-center justify-center'>
+          <motion.div variants={fadeIn('down', 0.5)} initial="hidden" whileInView={'show'} className='flex-1 lg:max-w-[500px] items-center justify-center transform-none'>
             {/* <img src={Image} alt="" /> */}
+            <div className=' mt-10'>
+              <div className="min-h-[100px] w-auto h-200 z-0 rounded-lg bg-dracula_background p-5 box-border" id='centerthis'>
+                <div className=" flex bg-transparent mb-3">
+                  <div className="w-5 h-5 z-30 rounded-full bg-dracula_red mr-3"></div>
+                  <div className="w-5 h-5 z-30 rounded-full bg-dracula_yellow mr-3"></div>
+                  <div className="w-5 h-5 z-30 rounded-full bg-dracula_green"></div>
+                </div>
+                <div>
+                  <div className='font-semibold'>
+                    <p className=' text-white'>[vskvj3@github] <TypeAnimation sequence={['', 2000, 'whoami', 2000]} wrapper='span' speed={45} className=' text-dracula_green' cursor={false} /> </p>
+                    <TypeAnimation sequence={['', 3000, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quo nesciunt laborum sapiente excepturi voluptas repellendus veritatis dolores dicta. Optio!', 2000]} speed={80} wrapper='p' cursor={false} />
+                  {isVisible &&  (<p className=' hidden-prompt'>[vskvj3@github] <TypeAnimation sequence={['', 2000, 'Nice to see you here!', 2000]} wrapper='span' speed={45} className=' text-dracula_green' /> </p>)}
 
+                  </div>
+                </div>
+                {/* Content goes here */}
+              </div>
+            </div>
           </motion.div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,16 @@ module.exports = {
       colors: {
         primary: '#0a0a0a',
         accent: '#B809C3',
+        dracula_yellow: '#f1fa8c',
+        dracula_red: '#ff5555',
+        dracula_purple: '#bd93f9',
+        dracula_pink: '#ff79c6',
+        dracula_green: '#50fa7b',
+        dracula_cyan: '#8be9fd',
+        dracula_comment: '#6272a4',
+        dracula_foreground: '#f8f8f2',
+        dracula_currentline: '#44475a',
+        dracula_background: '#282a36'
       },
       backgroundImage: {
         site: "url('./assets/site-bg.jpg')",
