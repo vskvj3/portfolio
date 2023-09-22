d2418609a557319b8a0aba15246fdfc76096bdfa
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2023-09-22 17:32:23 +0530

    feat: add resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Logo from '../assets/logo.png';
 
 const Header = () => {
+  const getResume = () => {
+    window.location.href = 'https://drive.google.com/file/d/1__v8HIBDno8SqG4fMW6qzLrbvWabGMxI/view?usp=sharing';
+  };
+
   return <header className=' py-8 '>
     <div className="container mx-auto">
       <div className='flex justify-between items-center'>
@@ -9,7 +13,7 @@ const Header = () => {
           <img src={Logo} alt="" width={100} height={100}/>
         </a>
 
-        <button className='btn btn-sm'>
+        <button className='btn btn-sm' href='google.com' onClick={getResume}>
           Resume
         </button>
       </div>
