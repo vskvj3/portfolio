0d22b941a357d9210994e7a64ed5a27911911b73
vskvj3 <92223768+vskvj3@users.noreply.github.com>
2023-09-29 21:29:17 +0530

    fix: project section is updated

--- src/components/Work.js
+++ src/components/Work.js
@@ -29,7 +29,7 @@ const Work = () => {
               {/* image */}
               <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
                 {/* overlay  */}
-                <div className='group-hover:bg-black/80 bg-black/50 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <div className='group-hover:bg-black/90 bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
                 {/* img */}
                 <img className='group-hover:scale-125 transition-all duration-500' src={img1} alt="" />
                 {/* before hover */}
@@ -38,7 +38,7 @@ const Work = () => {
                     <span className='font-semibold text-xl lg:text-3xl text-white'>AlertMe</span>
                     <p className='text-sm lg:text-xl text-white mt-1'>Android application developed to send emergency alert to nearby users, emergency contacts and authorities.</p>
                   </div>
-                  <div className='flex-row absolute top-32 lg:top-40 group-hover:-top-full transition-all duration-300 left-2  lg:left-12  z-50'>
+                  <div className='flex-row absolute top-32 lg:top-44 group-hover:-top-full transition-all duration-300 left-2  lg:left-12  z-50'>
                     < Chips content={'Node.js'} />
                     < Chips content={'Flutter'} />
                     < Chips content={'MongoDB'} />
@@ -46,21 +46,26 @@ const Work = () => {
                     < Chips content={'JWT'} />
                     < Chips content={'FCM'} />
                   </div>
-
+                  {/* 
                   <div className='absolute top-56 group-hover:-top-full transition-all duration-300 left-12  z-50'>
                     <button className='btn px-10'>View Project</button>
                   </div>
+*/}
 
                 </div>
                 {/* pretitle */}
-                <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <div className='absolute -bottom-full left-6 lg:left-12 group-hover:lg:bottom-52 group-hover:bottom-28 transition-all duration-500 z-50'>
                   <span className='text-gradient'>
                     Academic Project
                   </span>
                 </div>
                 {/* title */}
-                <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-600 z-50'>
-                  <span className='text-3xl text-white'>Alertme</span>
+                <div className='absolute -bottom-full left-6 lg:left-12 group-hover:lg:bottom-60 group-hover:bottom-36 transition-all duration-600 z-50'>
+                  <span className='text-2xl lg:text-3xl text-white'>Alertme</span>
+                </div>
+
+                <div className='absolute -bottom-full left-6 lg:left-12 group-hover:lg:bottom-24 group-hover:bottom-10 transition-all duration-600 z-50'>
+                  <button className='btn btn-sm sm:text-sm'>View Project</button>
                 </div>
               </div>
             </motion.div>
@@ -71,7 +76,7 @@ const Work = () => {
               {/* image  */}
               <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
                 {/* overlay  */}
-                <div className='group-hover:bg-black/80 w-full h-full bg-black/50 absolute z-40 transition-all duration-300'></div>
+                <div className='group-hover:bg-black/90 w-full h-full bg-black/70 absolute z-40 transition-all duration-300'></div>
                 {/* img */}
                 <img className='group-hover:scale-125 transition-all duration-500' src={img2} alt="" />
                 {/* before hover */}
@@ -80,7 +85,7 @@ const Work = () => {
                     <span className=' font-semibold text-xl lg:text-3xl text-white'>Hostel Manager</span>
                     <p className='text-sm lg:text-xl text-white mt-1'>A software developed to monitor daily attendance, track expanses and calculate mess bill for a hostel.</p>
                   </div>
-                  <div className='flex-row absolute top-36 lg:top-32 group-hover:-top-full transition-all duration-300 left-2 lg:left-12  z-50'>
+                  <div className='flex-row absolute top-32 lg:top-44 group-hover:-top-full transition-all duration-300 left-2 lg:left-12  z-50'>
                     < Chips content={'Java'} />
                     < Chips content={'MySQL'} />
                     < Chips content={'JDBC'} />
@@ -88,22 +93,22 @@ const Work = () => {
                     < Chips content={'NetBeans IDE'} />
                   </div>
 
-                  <div className='absolute top-56 group-hover:-top-full transition-all duration-300 left-12  z-50'>
-                    <button className='btn px-10'>View Project</button>
-                  </div>
-
                 </div>
                 {/* after hover  */}
                 <div>
                   {/* pretitle */}
-                  <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                  <div className='absolute -bottom-full left-6 lg:left-12 group-hover:lg:bottom-52 group-hover:bottom-28 transition-all duration-500 z-50'>
                     <span className='text-gradient'>
                       Academic Project
                     </span>
                   </div>
                   {/* title */}
-                  <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-600 z-50'>
-                    <span className='text-3xl text-white'>Hostel Manager</span>
+                  <div className='absolute -bottom-full left-6 lg:left-12 group-hover:lg:bottom-60 group-hover:bottom-36 transition-all duration-600 z-50'>
+                    <span className='text-2xl lg:text-3xl text-white'>Hostel Manager</span>
+                  </div>
+
+                  <div className='absolute -bottom-full left-6 lg:left-12 group-hover:lg:bottom-24 group-hover:bottom-10 transition-all duration-600 z-50'>
+                    <button className='btn btn-sm sm:text-sm'>View Project</button>
                   </div>
                 </div>
               </div>
@@ -111,7 +116,7 @@ const Work = () => {
             <motion.div variants={fadeIn('left', 0.2)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.3 }} className=''>
               <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
                 {/* overlay  */}
-                <div className='group-hover:bg-black/80 bg-black/50 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <div className='group-hover:bg-black/90 bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
                 {/* img */}
                 <img className='group-hover:scale-125 transition-all duration-500' src={img3} alt="" />
                 {/* before hover */}
@@ -120,28 +125,26 @@ const Work = () => {
                     <span className=' font-semibold text-xl sm:text-3xl text-white'>Ambience</span>
                     <p className='text-sm lg:text-xl text-white mt-1'>An android application developed to listen audiobooks from streaming services with advanced controlls and background sound functionality.</p>
                   </div>
-                  <div className='flex-row absolute top-32 lg:top-40 group-hover:-top-full transition-all duration-300 left-2 lg:left-12  z-50'>
-                    < Chips content={'Java'} />
-                    < Chips content={'MySQL'} />
-                    < Chips content={'JDBC'} />
-                    < Chips content={'Swing'} />
-                    < Chips content={'NetBeans IDE'} />
+                  <div className='flex-row absolute top-32 lg:top-44 group-hover:-top-full transition-all duration-300 left-2 lg:left-12  z-50'>
+                    < Chips content={'Flutter'} />
+                    < Chips content={'Android'} />
                   </div>
 
-                  <div className='absolute top-56 group-hover:-top-full transition-all duration-300 left-12  z-50'>
-                    <button className='btn px-10'>View Project</button>
-                  </div>
 
                 </div>
                 {/* pretitle */}
-                <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <div className='absolute -bottom-full left-6 lg:left-12 group-hover:lg:bottom-52 group-hover:bottom-28 transition-all duration-500 z-50'>
                   <span className='text-gradient'>
                     Hobby Project
                   </span>
                 </div>
                 {/* title */}
-                <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-600 z-50'>
-                  <span className='text-3xl text-white'>Ambience</span>
+                <div className='absolute -bottom-full left-6 lg:left-12 group-hover:lg:bottom-60 group-hover:bottom-36 transition-all duration-600 z-50'>
+                  <span className='text-2xl lg:text-3xl text-white'>Ambience</span>
+                </div>
+
+                <div className='absolute -bottom-full left-6 lg:left-12 group-hover:lg:bottom-24 group-hover:bottom-10 transition-all duration-600 z-50'>
+                  <button className='btn btn-sm sm:text-sm'>View Project</button>
                 </div>
               </div>
             </motion.div>
